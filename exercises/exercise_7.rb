123502b4660d939580c9d88cc8ea109b076bf731
Linh Hoang <ellehoang@hotmail.com>
2022-01-12 03:02:06 -0500

    implement validations for Store and Employee model and Store model

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# ask user for store name (store it in varible)
+puts "Please enter a store name"
+store_name = gets.chomp
+
+# attempt to create a store with the inputted name, but leave out other fields (annual_revenue, mens_apparel, womens_apparel)
+create_store = Store.create(name: store_name)
+
+# display error msg provided back from ActiveRecord to user (one on each line) after attempt to save/create record
+puts create_store.errors.messages
