4d5464c25ed696624da6f3d26b9f4978a6c3f657
Linh Hoang <ellehoang@hotmail.com>
2022-01-12 00:52:43 -0500

    assign the first and second stores to an instance variable and update and instance of the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Load first store (id = 1) from database and assign it to an instance variable @store1
+@store1 = Store.find_by(id: 1)
+# 2. Load second store from database and assign it to @store2
+@store2 = Store.find_by(id: 2)
+# 3. Update first store (@store1) instance in database. (Change its name or something)
+@store1.update(mens_apparel: false)
+
+# puts Store.first.inspect
