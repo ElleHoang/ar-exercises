368aec29cd354055c7728bd0802402ea0d4958d3
Linh Hoang <ellehoang@hotmail.com>
2022-01-12 02:16:21 -0500

    implement a one-to-many asscociation between Employee class and Store class, and create datas for employees at two stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Ann", last_name: "Perkin", hourly_rate: 16)
+@store1.employees.create(first_name: "Ron", last_name: "Swanson", hourly_rate: 23)
+@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 31)
+@store2.employees.create(first_name: "Clark", last_name: "Kent", hourly_rate: 22)
