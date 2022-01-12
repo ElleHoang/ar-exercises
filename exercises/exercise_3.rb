aa221d7f06411cb0dbc53789ff2bf237f97ccd88
Linh Hoang <ellehoang@hotmail.com>
2022-01-12 01:02:16 -0500

    assign the third store to an instance variable and delete the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. load third store (into @store3)
+@store3 = Store.find_by(id: 3)
+# 2. using Active Record's [destory] method, delete the store from database
+@store3.destroy
+# 3. verify that store has been deleted by outputing [count]
+puts Store.count
+#puts Store.all.inspect
